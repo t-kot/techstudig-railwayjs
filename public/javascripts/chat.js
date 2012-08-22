d538b041cffef14b4a2c34cbf4b400a45a9167a8
kotohata-imac <t.kotohata@gmail.com>
2012-08-22 21:59:31 +0900

    chat.js is created for seperate js lib

---
+++ public/javascripts/chat.js
@@ -0,0 +1,26 @@
+var host = location.href;
+var socket = io.connect("127.0.0.1:3000");
+
+//At connecting server
+socket.on('connect', function(msg){
+  $('#connectId').html("あなたの接続番号"+socket.socket.transport.sessid);
+});
+
+//At receiving message
+socket.on('message', function(msg){
+  console.log(msg.value);
+  $("#receiveMsg").append(msg.value+"<br />");
+});
+
+//At sending message
+function sendMsg(){
+  var msg = $('#message').val();
+  socket.emit('message', {value:msg});
+}
+
+//At disconnecting 
+function disConnect(){
+  var msg = socket.socket.transport.sessid + 'は切断しました';
+  socket.emit('message', { value: msg});
+  socket.disconnect();
+}
